6d81b852a501ad6c5f41ebd81306c39e714db9f7
Yaseen <abd181103@gmail.com>
2025-03-23 09:51:04 +0300

    Add mode files

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const PORT = 3000;
 
 // API route
 app.get('/api/message', (req, res) => {
-  res.json({ message: 'Hello from the Node API!',data: {name: 'John Doe', age: 30} });
+  res.json({ message: 'Hello from the Node API!',data: [
+    {name: 'John Doe', age: 30},
+    {name: 'Jane Doe', age: 25},
+    {name: 'Jim Doe', age: 35},
+] });
 });
 
 // Start server
